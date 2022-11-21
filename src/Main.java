a4e7d68070d1b1475ff6ddfce94b63e973f93934
mevshnu <vishnusivan1000@gmail.com>
2022-11-21 11:10:27 +0530

    end

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,21 @@ public class Main {
     static void add(int x,int y)
     {
         int a = x + y ;
+        System.out.println(+a);
     }
     static void sub(int x,int y) {
         int b = x - y;
+        System.out.println(+b);
     }
     static void mul(int x, int y)
     {
         int c = x * y;
+        System.out.println(+c);
     }
     static void div(int x, int y)
     {
         float c = x / y;
+        System.out.println(+c);
     }
     public static void main(String[] args)
     {
@@ -26,7 +30,21 @@ public class Main {
         System.out.println("enter 2 for subtraction");
         System.out.println("enter 3 for multiplication");
         System.out.println("enter 4 for division");
+        System.out.println("enter 5 for exit");
         int z = sc.nextInt();
+        switch (z)
+        {
+            case 1:add(x,y);
+                break;
+            case 2:sub(x,y);
+                break;
+            case 3:mul(x,y);
+                break;
+            case 4:div(x,y);
+                break;
+            case 5:System.exit(0);
+                break;
+        }
 
     }
 }
